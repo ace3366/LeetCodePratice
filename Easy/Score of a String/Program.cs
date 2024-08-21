02e917d66c1e8e502230a3d64cc904f20d24307f
ace3366 <poohmandu22@gmail.com>
2024-08-21 14:55:52 +0700

    Solution(#3):Easy

---
+++ Easy/Score of a String/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int ScoreOfString(string s)
+{
+    var convertedString = new List<int>();
+    // Convert to ASCII
+    foreach (var c in s)
+    {
+        convertedString.Add((int)c);
+    }
+    // Get Score of String
+    var total = 0;
+    for (int i = 0; i < convertedString.Count; i++)
+    {
+        total += i == convertedString.Count - 1 ? 0 :
+            Math.Abs(convertedString[i + 1] - convertedString[i]);
+    }
+    return total;
+}
+
+var inputString = "zaz";
+Console.WriteLine(ScoreOfString(inputString));
