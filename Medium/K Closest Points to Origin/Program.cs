44f38b34d0223e72b7f344ebc0bdcc699db5a94d
ace3366 <poohmandu22@gmail.com>
2024-08-22 14:32:13 +0700

    solution(#2):Medium

---
+++ Medium/K Closest Points to Origin/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.WebSockets;
+
+int[][] KClosest(int[][] points, int k)
+{
+    Dictionary<int, double> distances = new Dictionary<int, double>();
+    List<int[]> closest = new List<int[]>();
+    for (int i = 0; i < points.Length; i++)
+    {
+        distances.Add(i, Math.Sqrt(points[i][0] * points[i][0] + points[i][1] * points[i][1]));
+    }
+
+    var sortedList = distances.OrderBy(distance => distance.Value).ToArray();
+    for (int i = 0; i <= k; i++)
+    {
+        closest.Add(points[sortedList[i].Key]);
+    }
+    return closest.ToArray();
+}
+
+KClosest([[3, 3], [5, -1], [-2, 4]], 2);
