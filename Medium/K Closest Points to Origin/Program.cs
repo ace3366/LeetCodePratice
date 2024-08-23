bd729de535bf7a39330249451982b7b6b3e67f82
ace3366 <poohmandu22@gmail.com>
2024-08-23 13:12:35 +0700

    Solution(#3):Medium

--- Medium/K Closest Points to Origin/Program.cs
+++ Medium/K Closest Points to Origin/Program.cs
@@ -4,13 +4,15 @@ int[][] KClosest(int[][] points, int k)
 {
     Dictionary<int, double> distances = new Dictionary<int, double>();
     List<int[]> closest = new List<int[]>();
+    // Get the calculated value and the index is the key
     for (int i = 0; i < points.Length; i++)
     {
         distances.Add(i, Math.Sqrt(points[i][0] * points[i][0] + points[i][1] * points[i][1]));
     }
-
+    // Order the dictionary
     var sortedList = distances.OrderBy(distance => distance.Value).ToArray();
-    for (int i = 0; i <= k; i++)
+    // Return k point
+    for (int i = 0; i < k; i++)
     {
         closest.Add(points[sortedList[i].Key]);
     }
