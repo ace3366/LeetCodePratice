b199debf1555d8202e1ab3521f9498a2b10b0488
ace3366 <poohmandu22@gmail.com>
2024-09-09 09:50:01 +0700

    chore:small issue

--- tests/AlgorithmPractice.UnitTests/StringScoreCalculator/StringScoreTests.cs
+++ tests/AlgorithmPractice.UnitTests/StringScoreCalculator/StringScoreTests.cs
@@ -10,7 +10,7 @@ namespace AlgorithmPractice.UnitTests.StringScoreCalculator
         [Theory]
         [InlineData("abc", 2)]
         [InlineData("xyz", 2)]
-        [InlineData("hello", 7)]
+        [InlineData("hello", 13)]
         [InlineData("A", 0)]
         [InlineData("", 0)]
         public void StringScoreCaculator_InputAnyString_ShouldReturnExpectedResult(string input, int expectedResult)
